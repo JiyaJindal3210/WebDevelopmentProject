217936d6f3b1190f11de947054ff7ca0c9cae7c6
JiyaJindal3210 <jiyajindal27@gmail.com>
2025-02-19 12:52:57 +0530

    Done

--- app.py
+++ app.py
@@ -1,9 +1,78 @@
 from flask import Flask, render_template, redirect, request, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_bcrypt import Bcrypt
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app=Flask(__name__)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + \
+    os.path.join(basedir, "app.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+
+
+app.config["SECRET_KEY"] = "Your secret key"
+
+
+
+
+db = SQLAlchemy(app)
+
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+
+
+login_manager.init_app(app)
+
+
+login_manager.login_view = "login"
+
+
+class User(db.Model, UserMixin):
+
+
+    __tablename__ = "user"
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password_hash = db.Column(db.String(100), nullable=False)
+    mobile = db.Column(db.String(15), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default="user")
+
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(User, int(user_id))
+
+
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
     return  render_template("index.html")
+
+
 
 @app.route('/contact')
 def contact():
